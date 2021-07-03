1630cace9f28539d00b51d1bbe6e1a7745ffa806
Ed Hernandez <help.uds@gmail.com>
2021-07-03 09:07:43 -0700

    Mongoose Population

--- models/campsite.js
+++ models/campsite.js
@@ -17,8 +17,8 @@ const commentSchema = new Schema(
       required: true,
     },
     author: {
-      type: String,
-      require: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     },
   },
   {
