eab97ad780f36b5a5b57a1410bc5f5394b5d4475
Ed Hernandez <help.uds@gmail.com>
2021-07-03 08:54:35 -0700

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
-const passport = require('passport');
+const passport = require("passport");
+const authenticate = require("../authenticate");
 
 const router = express.Router();
 
@@ -11,28 +12,29 @@ router.get("/", function (req, res, next) {
 
 router.post("/signup", (req, res) => {
   User.register(
-    new User({username: req.body.username}),
+    new User({ username: req.body.username }),
     req.body.password,
-    err => {
+    (err) => {
       if (err) {
         res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({err: err});
+        res.setHeader("Content-Type", "application/json");
+        res.json({ err: err });
       } else {
-        passport.authenticate('local')(req, res, () => {
+        passport.authenticate("local")(req, res, () => {
           res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful!'});
+          res.setHeader("Content-Type", "application/json");
+          res.json({ success: true, status: "Registration Successful!" });
         });
       }
     }
   );
 });
 
-router.post("/login", passport.authenticate('local'), (req, res) => {
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  const token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.setHeader("Content-Type", "application/json");
+  res.json({ success: true, token: token, status: "You are successfully logged in!" });
 });
 
 router.get("/logout", (req, res, next) => {
