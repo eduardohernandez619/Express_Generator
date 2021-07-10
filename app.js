6ee388b62c0d4639b6d6aa2c20c90d4b02a6cb92
Ed Hernandez <help.uds@gmail.com>
2021-07-10 06:15:52 -0700

    File Upload

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var path = require("path");
 var logger = require("morgan");
 
 const passport = require("passport");
-const config = require('./config');
+const config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require("./routes/uploadRouter");
+
 const mongoose = require("mongoose");
 
 const url = config.mongoUrl;
@@ -28,14 +30,19 @@ connect.then(
 
 var app = express();
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   if (req.secure) {
     return next();
   } else {
-    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-    res.redirect(301,`https://${req.hostname}:${app.get('secPort')}${req.url}`)
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
   }
-})
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -46,10 +53,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-
-
 app.use(passport.initialize());
-
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -59,6 +63,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
