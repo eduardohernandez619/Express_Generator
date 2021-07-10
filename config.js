7bc1942d07b33d689cc9e05743e2ab66577c50da
Ed Hdez <eduardohernandez@sandiego.edu>
2021-07-10 08:22:01 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1216881765415878',
-        clientSecret: '5bd32f9717151e5def927701879267a4'
-    }
-}
