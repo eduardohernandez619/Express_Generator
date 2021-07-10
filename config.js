de9a74d48cd0bd14a1297cbf1a4fd3e1cadbd947
Ed Hernandez <help.uds@gmail.com>
2021-07-10 08:01:08 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1216881765415878',
+        clientSecret: '5bd32f9717151e5def927701879267a4'
+    }
 }
